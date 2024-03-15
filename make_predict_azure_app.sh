9a8e6fc331090ec64137c8eeea73fcae74527de9
nil-github <66524063+nil-github@users.noreply.github.com>
2024-03-15 16:48:57 +0530

    Update make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://myazureapponline15.azurewebsites.net:$PORT/predict 
+     -X POST https://seemyazureapponline15.azurewebsites.net:$PORT/predict 
      # TODO: Replace the <yourappname> with your application name 
